ab22f31629a93134b0e595ab43cc57c482adaa48
djtwdix <djtwd.ix@icloud.com>
2021-05-03 08:02:27 -0300

    Ogranize file structure

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDW5bB-0ReZ8g6s3DmBAXeUZtrw6RUL-VA",
-  authDomain: "netflix-clone-djtwdix.firebaseapp.com",
-  projectId: "netflix-clone-djtwdix",
-  storageBucket: "netflix-clone-djtwdix.appspot.com",
-  messagingSenderId: "83091332732",
-  appId: "1:83091332732:web:32d80a57aa05530c9473f9"
-};
