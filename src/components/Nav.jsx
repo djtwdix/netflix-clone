65934cbf9029248931e0f4c948cebf1da3174d06
djtwdix <djtwd.ix@icloud.com>
2021-05-03 08:01:12 -0300

    Create components directory

---
+++ src/components/Nav.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+function Nav() {
+  const [show, setShow] = useState(false);
+
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+
+    return () => window.removeEventListener("scroll", transitionNavbar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo.png"
+          alt="logo"
+        />
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
