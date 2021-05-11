644107e859128421fc149acc60bff1d4ce298f92
djtwdix <djtwd.ix@icloud.com>
2021-05-11 10:57:25 -0300

    Change prop to NetflixOriginals

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -14,7 +14,7 @@ function HomeScreen() {
         fetchURL={requests.fetchNetflixOriginals}
         isLargeRow
       />
-      <Row title="Trending Now" fetchURL={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
       <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
       <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
