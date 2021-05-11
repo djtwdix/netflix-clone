cb49b7e786fe128f228cb59b2a368fda66058294
djtwdix <djtwd.ix@icloud.com>
2021-05-11 10:57:07 -0300

    Add row item and remove large row code

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios/instance";
+import RowItem from "./RowItem";
 
-function Row({ title, fetchURL, isLargeRow = false }) {
+function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
 
   const baseURL = "https://images.tmdb.org/t/p/original/";
@@ -15,26 +16,13 @@ function Row({ title, fetchURL, isLargeRow = false }) {
     fetchData();
   }, [fetchURL]);
 
-  console.log(movies);
-
   return (
     <div className="row">
       <h2 className="row__title">{title}</h2>
       <div className="row__posters">
-        {movies.map(
-          (movie) =>
-            ((isLargeRow && movie.poster_path) ||
-              (!isLargeRow && movie.backdrop_path)) && (
-              <img
-                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                key={movie.id}
-                src={`${baseURL}${
-                  isLargeRow ? movie.poster_path : movie.backdrop_path
-                }`}
-                alt={movie?.name}
-              />
-            )
-        )}
+        {movies.map((movie) => (
+          <RowItem key={movie?.id} baseURL={baseURL} movie={movie} />
+        ))}
       </div>
     </div>
   );
