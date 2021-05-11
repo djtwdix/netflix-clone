00c8b70c090f7c1e9d7d4c44569920b11bffaf7d
djtwdix <djtwd.ix@icloud.com>
2021-05-11 10:58:14 -0300

    Remove whitespace

--- src/axios/requests.js
+++ src/axios/requests.js
@@ -1,12 +1,13 @@
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_networks=213`,
-  fetchTopRated: `movie/top_rated?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=99`,
+  fetchTrending: `/trending/all/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&append_to_response=videos`,
+  fetchNetflixOriginals: `discover/tv?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_networks=213&append_to_response=videos`,
+  fetchTopRated: `movie/top_rated?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&append_to_response=videos`,
+  fetchActionMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=28&append_to_response=videos`,
+  fetchComedyMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=35&append_to_response=videos`,
+  fetchHorrorMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=27&append_to_response=videos`,
+  fetchRomanceMovies: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=10749&append_to_response=videos`,
+  fetchDocumentaries: `/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&with_genres=99&append_to_response=videos`,
 };
 
 export default requests;
+
