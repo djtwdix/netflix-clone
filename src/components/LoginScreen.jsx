e9af899e61fb4ea7724ef975dd0bc19c1e939a9c
djtwdix <djtwd.ix@icloud.com>
2021-05-13 15:41:09 -0300

    Create login screen component

---
+++ src/components/LoginScreen.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import SignUpScreen from './SignUpScreen';
+
+function Login() {
+  const [signIn, setSignIn] = useState(false);
+
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen__background">
+        <img
+          className="loginScreen__logo"
+          src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo.png"
+          alt="logo"
+        />
+      </div>
+      <button onClick={() => setSignIn(true)} className="loginScreen__button">
+        Sign In
+      </button>
+      <div className="loginScreen__gradient" />
+      <div className="loginScreen__body">
+        {signIn ? (
+          <SignUpScreen />
+        ) : (
+          <>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel any time</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </h3>
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen__getStarted"
+                  type="submit"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
