c0dab571275f65516e93c018a729448764f851c8
djtwdix <djtwd.ix@icloud.com>
2021-05-13 15:40:20 -0300

    Add auth and db to firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,18 @@
+import firebase from "firebase";
+import "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDW5bB-0ReZ8g6s3DmBAXeUZtrw6RUL-VA",
   authDomain: "netflix-clone-djtwdix.firebaseapp.com",
   projectId: "netflix-clone-djtwdix",
   storageBucket: "netflix-clone-djtwdix.appspot.com",
   messagingSenderId: "83091332732",
-  appId: "1:83091332732:web:32d80a57aa05530c9473f9"
+  appId: "1:83091332732:web:32d80a57aa05530c9473f9",
 };
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const auth = app.auth();
+const db = app.firestore();
+
+export { auth, db };
