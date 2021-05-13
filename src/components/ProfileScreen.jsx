ee0bc2fe71dd4c96641bc27ed3dc209d6cbf5959
djtwdix <djtwd.ix@icloud.com>
2021-05-13 15:40:35 -0300

    Create profile screen component

---
+++ src/components/ProfileScreen.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfileScreen() {
+  return (
+    <div className="profileScreen">
+      <h1>Profile Screen</h1>
+    </div>
+  );
+}
+
+export default ProfileScreen;
