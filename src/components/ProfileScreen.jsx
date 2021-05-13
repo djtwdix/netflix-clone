620efd58605ab9bcfd864b4394f448c4e6fee003
djtwdix <djtwd.ix@icloud.com>
2021-05-13 16:28:03 -0300

    Start build and style of profile screen

--- src/components/ProfileScreen.jsx
+++ src/components/ProfileScreen.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import Nav from "./Nav";
+import { auth } from "../firebase/config";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 
 function ProfileScreen() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="profileScreen">
-      <h1>Profile Screen</h1>
+      <Nav />
+      <div className="profileScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt="avatar"
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>Plans</h3>
+              <button
+                className="profileScreen__signOut"
+                onClick={() => auth.signOut()}
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
